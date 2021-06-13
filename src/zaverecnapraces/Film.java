7c4386ba5f5160fdad960c7b77288b787764dc1a
sunjecna <sun@spsejecna.cz>
2021-06-13 17:14:49 +0200

    Final Changes

--- src/zaverecnapraces/Film.java
+++ src/zaverecnapraces/Film.java
@@ -47,7 +47,7 @@ public class Film implements Comparable<Film> {
     public String toString() { 
     	return "Film: " + Nadpis +"\n" +
                "Kinosal: " + Kinosal + "\n" +
-    			"Cas: " + Cas + "\n" +
+    			"Cas: " + Cas + "\n" + gay
     			"Zanr: " + Zanr + "\n" +
     			"Herci: " + Herci + "\n";
     			
